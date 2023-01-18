d46bfcc606b7a2dd7c222712624334ff50a07b12
Jay Do <jayandn1999@gmail.com>
2023-01-18 20:41:34 +0700

    feat: :sparkles: Login by Metamask now available! Total: 49 hours

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -5,6 +5,7 @@ const router = Router();
 
 router.post('/signup', authController.signup_post);
 router.post('/google/signup', authController.signupGoogle_post);
+router.post('/metamask/signup', authController.signupMetamask_post);
 router.post('/login', authController.login_post);
 router.get('/logout', authController.logout_get);
 
