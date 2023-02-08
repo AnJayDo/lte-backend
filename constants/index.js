3e65289db7ce1bcd269722dde83abcf943ea360e
Jay Do <jayandn1999@gmail.com>
2023-02-08 18:41:38 +0700

    chore: :memo: Update APY per 5 minutes

--- constants/index.js
+++ constants/index.js
@@ -17,6 +17,6 @@ const APY_YEAR = 3;
 
 const APY_DAY = 1.06108;
 
-const APY_5_MINUTES = 1.006303113;
+const APY_5_MINUTES = 1.0002023431;
 
 module.exports = { INIT_USER, GAIN_POINT, APY_DAY, APY_5_MINUTES };
